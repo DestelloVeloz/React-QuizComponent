512c084285c8fef55c8ea47f46064d43310973a2
DestelloVeloz <40174677@live.napier.ac.uk>
2018-09-17 23:37:18 +0100

    created a QuizEnd Component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
 //setting equal the variable to the contents of a data file
 let quizData = require('./quiz_data.json')
@@ -11,10 +12,13 @@ class Quiz extends Component {
     this.state = {quiz_position: 1 }
   }
   render() {
+    const isQuizEnd = ((this.state.quiz_position -1)) === quizData.quiz_questions.length)
+    //a condition that checks the value of its quiz that displays the QuizEnd component if true and the QuizQuestion component if false
     return (
       <div>
+        {isQuizEnd ? <QuizEnd /> :
         <QuizQuestion quiz_question={quizData.quiz_questions
-          [this.state.quiz_position - 1]}/>
+          [this.state.quiz_position - 1]}/>}
       </div>
     )
   }
